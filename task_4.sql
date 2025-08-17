aeca09fc3d3b765d648ebfa167616b18d6b1d48e
euphraxiia <ST10433587@rcconnect.edu.za>
2025-08-17 10:19:57 +0200

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,3 +1,4 @@
 -- Show full description of the books table from alx_book_store database
-USE alx_book_store;
-SHOW COLUMNS FROM Books;
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Books';
