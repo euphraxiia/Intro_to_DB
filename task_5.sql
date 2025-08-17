7e894a7272171eb8870907b8e2fe75c2ad73f997
euphraxiia <ST10433587@rcconnect.edu.za>
2025-08-17 10:21:04 +0200

    Create task_5.sql

---
+++ task_5.sql
@@ -0,0 +1,4 @@
+-- Insert a single customer record into the customers table
+USE alx_book_store;
+INSERT INTO Customers (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
